7685e47e67e2758fd3230d52a510ded570df683a
sec-li-n <loveuallnbs0205@yahoo.co.jp>
2021-01-02 07:59:41 +0900

    issue_vue_js

---
+++ issue_vue.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    // 要宣言
+    name: '',
+    course: '',
+    period: '',
+    students: [
+      { name: 'モモ', course:'web', period:'2020年11月' },
+    ]
+  },
+  methods: {
+    addStudent: function(){
+      this.students.push({name: this.name, course: this.course, period: this.period})
+
+    }
+  }
+})
